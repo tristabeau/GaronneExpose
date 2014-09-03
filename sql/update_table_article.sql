9fb16f0a37f0412ed3ce8341b528473928524175
tristabeau <tristabeau@free.fr>
2014-09-03 07:59:08 +0200

    feat(ajout du sql)

--- sql/update_table_article.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE article ADD corrige           TINYINT(1) DEFAULT FALSE NOT NULL;
-ALTER TABLE article ADD rapport           TEXT NOT NULL;
-ALTER TABLE article ADD correcteur        VARCHAR(50) NOT NULL;
